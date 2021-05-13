c10b8a746dd52e5c14a87efcd30bd0f02024c6a3
jodelvecchio <jose.delvecchio@wolox.co>
2021-05-13 16:27:42 -0500

    Adding javadoc

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -17,6 +17,10 @@ import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
 
+/**
+ * @author josefranciscodelvecchio
+ * Controller for CRUD Book
+ */
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -24,28 +28,53 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    /**
+     * This method find all Books
+     * @return {@link Book}
+     */
     @GetMapping
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
+    /**
+     * This method find books that match with title param
+     * @param title: Title of the Book
+     * @return {@link List}<{@link Book}>
+     */
     @GetMapping("/title/{title}")
     public List<Book> findByTitle(@PathVariable String title) {
         return bookRepository.findByTitle(title);
     }
 
+    /**
+     * This method find book by id param
+     * @param id: Id of the book
+     * @return {@link Book}
+     * @throws BookNotFoundException when book not found
+     */
     @GetMapping("/{id}")
     public Book findOne(@PathVariable Long id) {
         return bookRepository.findById(id)
                 .orElseThrow(BookNotFoundException::new);
     }
 
+    /**
+     * This method creates an {@link Book} with the data from book param
+     * @param book {@link Book}: Data to create
+     * @return created {@link Book}
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
         return bookRepository.save(book);
     }
 
+    /**
+     * This method delete book by id param
+     * @param id: Id of the book
+     * @throws BookNotFoundException when book to delete not found
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         bookRepository.findById(id)
@@ -53,6 +82,14 @@ public class BookController {
         bookRepository.deleteById(id);
     }
 
+    /**
+     * This method update data an book
+     * @param book
+     * @param id: Id of the book
+     * @return {@link Book}: Data to update
+     * @throws BookIdMismatchException when id param and book's id mismatch
+     * @throws BookNotFoundException when book not found
+     */
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
         if (book.getId() != id) {
--- src/main/java/wolox/training/controllers/GreetingController.java
+++ src/main/java/wolox/training/controllers/GreetingController.java
@@ -5,9 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * @author josefranciscodelvecchio
+ * Controller for greeting view
+ */
 @Controller
 public class GreetingController {
 
+    /**
+     * View with template greeting.html
+     * @param name
+     * @param model
+     * @return {@link String}
+     */
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
