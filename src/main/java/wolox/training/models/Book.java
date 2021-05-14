92431f8524da4a94185a6410e7650ffa76952b2d
jodelvecchio <jose.delvecchio@wolox.co>
2021-05-14 17:57:36 -0500

    Check not null objects with preconditions

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -1,5 +1,6 @@
 package wolox.training.models;
 
+import com.google.common.base.Preconditions;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -55,4 +56,48 @@ public class Book {
     @ManyToMany(mappedBy = "books")
     private List<User> users;
 
+    public void setGenre(String genre) {
+        Preconditions.checkNotNull(genre,"Please check the genre supplied, its null!");
+        this.genre = genre;
+    }
+
+    public void setAuthor(String author) {
+        Preconditions.checkNotNull(author,"Please check the author supplied, its null!");
+        this.author = author;
+    }
+
+    public void setImage(String image) {
+        Preconditions.checkNotNull(image,"Please check the image supplied, its null!");
+        this.image = image;
+    }
+
+    public void setTitle(String title) {
+        Preconditions.checkNotNull(title,"Please check the title supplied, its null!");
+        this.title = title;
+    }
+
+    public void setSubtitle(String subtitle) {
+        Preconditions.checkNotNull(subtitle,"Please check the subtitle supplied, its null!");
+        this.subtitle = subtitle;
+    }
+
+    public void setPublisher(String publisher) {
+        Preconditions.checkNotNull(publisher,"Please check the publisher supplied, its null!");
+        this.publisher = publisher;
+    }
+
+    public void setYear(String year) {
+        Preconditions.checkNotNull(year,"Please check the year supplied, its null!");
+        this.year = year;
+    }
+
+    public void setPages(Integer pages) {
+        Preconditions.checkNotNull(pages,"Please check the pages supplied, its null!");
+        this.pages = pages;
+    }
+
+    public void setIsbn(String isbn) {
+        Preconditions.checkNotNull(isbn,"Please check the isbn supplied, its null!");
+        this.isbn = isbn;
+    }
 }
