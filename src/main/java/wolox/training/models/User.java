625b74aa2c5f549ebca2844bbc59cb66dd3a08ae
jodelvecchio <jose.delvecchio@wolox.co>
2021-05-18 10:29:15 -0500

    clean code and use static import

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -1,6 +1,7 @@
 package wolox.training.models;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
@@ -18,8 +19,9 @@ import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
+import org.springframework.util.CollectionUtils;
 import wolox.training.exceptions.BookAlreadyOwnedException;
-import wolox.training.utils.Constants;
+import static wolox.training.utils.Constants.*;
 
 /**
  * Model class from table User
@@ -79,25 +81,27 @@ public class User {
     }
 
     public void setUserName(String userName) {
-        Preconditions.checkNotNull(userName,String.format(Constants.MESSAGE_CHECK_IS_NULL,"userName"));
-        Preconditions.checkArgument(!userName.isEmpty(),String.format(Constants.MESSAGE_CHECK_IS_EMPTY,"userName"));
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(userName),
+                String.format(MESSAGE_CHECK_IS_NULL_EMPTY, "userName"));
         this.userName = userName;
     }
 
     public void setName(String name) {
-        Preconditions.checkNotNull(name,String.format(Constants.MESSAGE_CHECK_IS_NULL,"name"));
-        Preconditions.checkArgument(!name.isEmpty(),String.format(Constants.MESSAGE_CHECK_IS_EMPTY,"name"));
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(name),
+                String.format(MESSAGE_CHECK_IS_NULL_EMPTY, "name"));
         this.name = name;
     }
 
     public void setBirthdate(LocalDate birthdate) {
-        Preconditions.checkNotNull(birthdate,String.format(Constants.MESSAGE_CHECK_IS_NULL,"birthdate"));
-        Preconditions.checkArgument(!birthdate.isBefore(LocalDate.now()),String.format(Constants.MESSAGE_CHECK_BEFORE_CURRENT_DATE,"birthdate"));
+        Preconditions.checkNotNull(birthdate,String.format(MESSAGE_CHECK_IS_NULL_EMPTY,"birthdate"));
+        Preconditions.checkArgument(!birthdate.isBefore(LocalDate.now()),
+                String.format(MESSAGE_CHECK_BEFORE_CURRENT_DATE,"birthdate"));
         this.birthdate = birthdate;
     }
 
     public void setBooks(List<Book> books) {
-        Preconditions.checkNotNull(books,String.format(Constants.MESSAGE_CHECK_IS_NULL,"books"));
+        Preconditions.checkArgument(!CollectionUtils.isEmpty(books),
+                String.format(MESSAGE_CHECK_IS_NULL_EMPTY,"books"));
         this.books = books;
     }
 }
--- src/main/java/wolox/training/utils/Constants.java
+++ src/main/java/wolox/training/utils/Constants.java
@@ -2,8 +2,7 @@ package wolox.training.utils;
 
 public class Constants {
 
-    public static final String MESSAGE_CHECK_IS_NULL = "Please check the %s supplied, its null!";
-    public static final String MESSAGE_CHECK_IS_EMPTY = "Please check the %s supplied, its empty!";
+    public static final String MESSAGE_CHECK_IS_NULL_EMPTY = "Please check the %s supplied, it cannot be null or empty";
     public static final String MESSAGE_CHECK_NOT_NUMBER = "Please check the %s supplied, must be number!";
     public static final String MESSAGE_CHECK_BEFORE_CURRENT_DATE = "Please check the %s supplied, must be before to current date!";
     public static final String MESSAGE_CHECK_GREATER_THAN_ZERO = "Please check the %s supplied, must be greater than zero!";
