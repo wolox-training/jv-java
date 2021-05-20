1b49151d52ee3ba5d6f353a85e9230b0d197c151
jodelvecchio <jose.delvecchio@wolox.co>
2021-05-19 23:25:50 -0500

    Unit test to layer MVC :white_check_mark:

---
+++ src/test/java/wolox/training/controllers/UserControllerTest.java
@@ -0,0 +1,90 @@
+package wolox.training.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import wolox.training.exceptions.UserNotFoundException;
+import wolox.training.models.User;
+import wolox.training.repositories.BookRepository;
+import wolox.training.repositories.UserRepository;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+    private User user;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        user = new User();
+        user.setUserName("jose.delvecchio");
+        user.setName("Jose");
+        user.setBirthdate(LocalDate.now().minusYears(26L));
+    }
+
+    @Test
+    public void whenFindOneWhichExists_thenUserIsReturned() throws Exception {
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(user));
+        final String url = "/api/users/1";
+        mvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userName",is("jose.delvecchio")));
+    }
+
+    @Test
+    public void whenFindAllUsers_thenReturnList() throws Exception {
+        Mockito.when(userRepository.findAll()).thenReturn(Arrays.asList(user));
+        final String url = "/api/users";
+        mvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void whenFindOneWhichNonExists_thenExceptionIsThrown() throws Exception {
+        final String url = "/api/users/10";
+        mvc.perform(get(url))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof UserNotFoundException));
+    }
+
+    @Test
+    public void whenCreateUser_thenReturnUserAdded() throws Exception {
+
+        final String url = "/api/users";
+
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated());
+    }
+
+}
